8d8be2586c3de36f90edd20415d9be1adcd785df
viswanthkj <viswanth1995@gmail.com>
2020-05-12 20:36:30 +0530

    changes: dynamic monster content-26

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state={
-      string: 'irundadu'
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json()).then(users => this.setState({ monsters: users}))
   }
   render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> nd save to reload.
-        </p>
-        <p>
-       {this.state.string}
-      </p>
-       <button onClick={()=> this.setState({ string: 'maathiten' })}>Change text</button>
-      </header>
+      {this.state.monsters.map((monster) => <h1 key={monster.id}>{monster.name}</h1>)}
     </div>
   );
   }
